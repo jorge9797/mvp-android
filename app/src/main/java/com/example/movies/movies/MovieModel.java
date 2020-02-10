0d683e8a50c0c85a77a126621e41c20d5bc3c366
Jorge De La Cruz <jorge-albertodelacruz@hotmail.com>
2020-02-09 22:01:42 -0600

    POJOS para consumo de Apis

--- app/src/main/java/com/example/movies/movies/MovieModel.java
+++ app/src/main/java/com/example/movies/movies/MovieModel.java
@@ -1,5 +1,7 @@
 package com.example.movies.movies;
 
+import com.example.movies.http.apimodel.Result;
+
 import io.reactivex.Observable;
 import io.reactivex.functions.BiFunction;
 
--- app/src/main/java/com/example/movies/movies/Result.java
+++
@@ -1,4 +0,0 @@
-package com.example.movies.movies;
-
-public class Result {
-}
