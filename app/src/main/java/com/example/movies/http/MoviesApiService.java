3846d11513133071cb53f57c8214348d7f826ec7
Jorge De La Cruz <jorge-albertodelacruz@hotmail.com>
2020-02-10 22:29:00 -0600

    Implementando Retrofit con claves de API

--- app/src/main/java/com/example/movies/http/MoviesApiService.java
+++ app/src/main/java/com/example/movies/http/MoviesApiService.java
@@ -10,7 +10,7 @@ import retrofit2.http.Query;
 public interface MoviesApiService {
 
     @GET("top_rated")
-    Observable<TopMoviesRated> getTopMoviesRated(@Query("api_key") String api_key , @Query("page") Integer page );
+    Observable<TopMoviesRated> getTopMoviesRated( @Query("page") Integer page );
 
 
 
